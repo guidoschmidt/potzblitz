1b12a8b548c71568606085de500541ec15dd8044
Guido Schmidt <git@guidoschmidt.cc>
2023-01-03 15:41:19 +0100

    EDIT/packages/components/react/Toggle: implement Toggle for React

--- packages/components/src/react/Toggle.tsx
+++ packages/components/src/react/Toggle.tsx
@@ -1,12 +1,26 @@
-import React from "react";
 import "../scss/Toggle.scss";
+import React, { useState, useEffect } from "react";
+import { camelCaseWithSpaces, ToggleProps } from "../api";
 
-export function Toggle() {
+export function Toggle(props: ToggleProps) {
+  const [vO, setVO] = useState<boolean>(props.value);
+
+  useEffect(() => {
+    setVO(props.value);
+  }, [props.value]);
+
+  const handleSwitch = () => {
+    const newValue = !vO;
+    setVO(newValue);
+    props.onChange && props.onChange(newValue);
+  };
+
   return (
     <div className="toggle">
-      <div className="knob">
-        <div className="state">
-          <input name="" type="checkbox" value="" />
+      {props.label && <label>{camelCaseWithSpaces(props.label)}</label>}
+      <div className="knob" onClick={handleSwitch}>
+        <div className={["state", vO && "on"].join(" ")}>
+          <input type="checkbox" value={vO} />
         </div>
       </div>
     </div>
