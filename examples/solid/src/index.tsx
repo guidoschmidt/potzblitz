eb775bdc0d7c5e65dc8f946afe1ad62ee84bd959
Guido Schmidt <git@guidoschmidt.cc>
2023-03-20 18:00:00 +0100

    FIX/examples/solid, ADD/packages/solid-components

--- examples/solid/src/index.tsx
+++ examples/solid/src/index.tsx
@@ -1,24 +1,25 @@
 import "./index.scss";
 import { render, For, Index } from "solid-js/web";
 import { createStore } from "solid-js/store";
-import {
-  ColorPicker,
-  ColorSelector,
-  Slider,
-  InputField,
-} from "@potzblitz/components/src/solid";
+import { ColorPicker, InputField, Button, Slider } from "@potzblitz/components";
 import { createSignal } from "solid-js";
+import { Counter } from "./Counter";
+import { Test, enableHydration } from "@potzblitz/solid-components/lib";
+
+enableHydration();
 
 function App() {
   const [store, updateStore] = createStore({ v: 10 });
   const [sliders, updateSliders] = createSignal([0, 0, 0]);
-  const [colors, updateColors] = createSignal(["", "", ""]);
+  const [counter, setCounter] = createSignal(0);
 
   return (
-    <div class="ui-root">
+    <div class="app">
       <h1>Solid × Potzblitz</h1>
+      <Counter value={counter()} onClick={() => setCounter(counter() + 1)} />
+      <Counter value={counter()} onClick={() => setCounter(counter() + 10)} />
       <ColorPicker value="#ff0000" />
-      <ColorSelector label="backgroundColor" value="#00ff00" />
+      <Test value={() => counter()} />
       <Slider
         value={store["v"]}
         min={0}
@@ -28,23 +29,13 @@ function App() {
       />
       <InputField value={store["v"]} min={0} max={20} step={0.01} />
 
-      <Index each={colors()}>
-        {(v, i) => {
-          return (
-            <div>
-              <ColorSelector label={`color-${i}`} />
-            </div>
-          );
-        }}
-      </Index>
-
       <Index each={sliders()}>
         {(v, i) => {
           return (
             <div>
               <Slider
                 label="Slider"
-                value={v}
+                value={v()}
                 onUpdate={(v: number) => {
                   const update = [...sliders()];
                   update[i] = v;
@@ -56,26 +47,34 @@ function App() {
           );
         }}
       </Index>
+
+      <Index each={sliders()}>
+        {(v, i) => {
+          return (
+            <div>
+              <Slider
+                label="Slider"
+                value={v()}
+                onUpdate={(v: number) => {
+                  const update = [...sliders()];
+                  update[i] = v;
+                  updateSliders(update);
+                }}
+              />
+              <span>Value: {v}</span>
+            </div>
+          );
+        }}
+      </Index>
+
+      <Button
+        label="Randomize"
+        onClick={() => {
+          updateSliders((old) => old.map((_) => Math.random() * 100));
+        }}
+      />
+
       <code>{JSON.stringify(sliders())}</code>
-
-      <Index each={sliders()}>
-        {(v, i) => {
-          return (
-            <div>
-              <Slider
-                label="Slider"
-                value={v}
-                onUpdate={(v: number) => {
-                  const update = [...sliders()];
-                  update[i] = v;
-                  updateSliders(update);
-                }}
-              />
-              <span>Value: {v}</span>
-            </div>
-          );
-        }}
-      </Index>
     </div>
   );
 }
