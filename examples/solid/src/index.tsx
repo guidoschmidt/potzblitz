2c872d7e8c99534b9a0e6eade453429c0a7cf0c8
Guido Schmidt <git@guidoschmidt.cc>
2023-03-20 17:41:29 +0100

    EDIT/examples/solid: integrate ColorSelector component

--- examples/solid/src/index.tsx
+++ examples/solid/src/index.tsx
@@ -1,7 +1,9 @@
+import "./index.scss";
 import { render, For, Index } from "solid-js/web";
 import { createStore } from "solid-js/store";
 import {
   ColorPicker,
+  ColorSelector,
   Slider,
   InputField,
 } from "@potzblitz/components/src/solid";
@@ -10,11 +12,13 @@ import { createSignal } from "solid-js";
 function App() {
   const [store, updateStore] = createStore({ v: 10 });
   const [sliders, updateSliders] = createSignal([0, 0, 0]);
+  const [colors, updateColors] = createSignal(["", "", ""]);
 
   return (
-    <div class="app">
+    <div class="ui-root">
       <h1>Solid × Potzblitz</h1>
       <ColorPicker value="#ff0000" />
+      <ColorSelector label="backgroundColor" value="#00ff00" />
       <Slider
         value={store["v"]}
         min={0}
@@ -23,6 +27,16 @@ function App() {
         onUpdate={(v) => updateStore("v", v)}
       />
       <InputField value={store["v"]} min={0} max={20} step={0.01} />
+
+      <Index each={colors()}>
+        {(v, i) => {
+          return (
+            <div>
+              <ColorSelector label={`color-${i}`} />
+            </div>
+          );
+        }}
+      </Index>
 
       <Index each={sliders()}>
         {(v, i) => {
@@ -43,6 +57,25 @@ function App() {
         }}
       </Index>
       <code>{JSON.stringify(sliders())}</code>
+
+      <Index each={sliders()}>
+        {(v, i) => {
+          return (
+            <div>
+              <Slider
+                label="Slider"
+                value={v}
+                onUpdate={(v: number) => {
+                  const update = [...sliders()];
+                  update[i] = v;
+                  updateSliders(update);
+                }}
+              />
+              <span>Value: {v}</span>
+            </div>
+          );
+        }}
+      </Index>
     </div>
   );
 }
