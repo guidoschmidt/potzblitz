fa983707e28f65ab81d28ce14f4ff31e5ae4e079
Guido Schmidt <git@guidoschmidt.cc>
2023-04-07 11:36:33 +0200

    FIX/examples/solid, ADD/packages/solid-components

--- examples/solid/vite.config.ts
+++ examples/solid/vite.config.ts
@@ -1,9 +1,9 @@
 // vite.config.js
-import solidPlugin from "vite-plugin-solid";
+import solid from "vite-plugin-solid";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solidPlugin({})],
+  plugins: [solid({})],
   build: {
     target: "esnext",
   },
